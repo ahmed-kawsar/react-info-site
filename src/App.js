960e9119b4c533d1bba8e49c065a73f8f8d2265a
ahmed-kawsar <kawsar4698@gmail.com>
2022-08-03 21:43:52 +0600

    set with components and started building react info site

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Content from './Content'
+import Header from './Header'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Header />
+      <Content />
+    </div>
+  )
+}
+
+export default App
